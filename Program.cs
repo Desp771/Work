45d1faa21c9fd1baac3a727534f5db9783116db8
Desp771 <Desp771@gmail.com>
2018-06-24 15:21:35 +0300

    Lesson 2

--- Program.cs
+++ Program.cs
@@ -8,8 +8,30 @@ namespace Snake
 {
     class Program
     {
+        static void draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x1, y1);
+            Console.Write(sym1);
+        }
+
         static void Main(string[] args)
         {
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+            draw(x1, y1, sym1);
+
+
+
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '#';
+            draw(x2, y2, sym2);
+
+
+
+            Console.ReadLine();
+
         }
     }
 }
